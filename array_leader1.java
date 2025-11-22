43f333ed317db014adce50072a92c28b64b4c205
Thoshinijalakam <thoshinijalakam@gmail.com>
2025-11-22 19:07:51 +0530

    Add files via upload

---
+++ array_leader1.java
@@ -0,0 +1,12 @@
+public class array_leader1{
+    public static void main(String[] args) {
+        int[] arr={19,10,12,6,4,3,9,4,5,3};
+        int max=0;
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i]>max){System.out.print(",");
+                max=arr[i];
+                System.out.println(max);
+            }
+        }
+    }
+}
