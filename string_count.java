de526d857d9e5148809ff092d0bfe513bb1abe5d
Thoshinijalakam <thoshinijalakam@gmail.com>
2025-11-22 19:14:25 +0530

    Add files via upload

---
+++ string_count.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class string_count{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        int count=1;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==' '&&str.charAt(i+1)!=' '){
+                count++;
+            }
+        }
+        System.out.println("the count of words:"+count);
+    }
+}
